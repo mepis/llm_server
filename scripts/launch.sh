edbd8c53e9b3b98cee0f6633de113db4d63afcc9
jon welling <jon.welling@ipqs.com>
2026-02-25 15:42:11 -0500

    Updated launch script, added kill script

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -7,7 +7,7 @@ cd llama.cpp
 cd build
 cd bin
 
-./llama-server -hf unsloth/gemma-3-4b-it-GGUF:Q8_0  \
+./llama-server -hf unsloth/Qwen3-Coder-Next-GGUF:Q4_0  \
   --port 4096 \
   --host 100.72.27.6 \
   --no-mmap \
@@ -16,5 +16,6 @@ cd bin
   --split-mode layer \
   --tensor-split 16,12,12 \
   --main-gpu 0 \
-  --spec-type ngram-mod --spec-ngram-size-n 24 --draft-min 48 --draft-max 64
+  --spec-type ngram-mod --spec-ngram-size-n 24 --draft-min 48 --draft-max 64 \
+  & disown
 
