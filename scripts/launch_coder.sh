2d5230512ed46278c52df164274b1954603c5948
jon welling <jon.welling@ipqs.com>
2026-02-25 21:37:23 -0500

    updated launch script

--- scripts/launch_coder.sh
+++ scripts/launch_coder.sh
@@ -7,15 +7,17 @@ cd llama.cpp
 cd build
 cd bin
 
-./llama-server -hf unsloth/Qwen3.5-35B-A3B-GGUF:Q6_K  \
+./llama-server -hf unsloth/Qwen3.5-35B-A3B-GGUF:UD-Q4_K_XL  \
   --port 4096 \
   --host 100.90.21.80 \
   --no-mmap \
    -c 64000 \
   -ngl 99 \
   --split-mode layer \
-  --tensor-split 16,12,12 \
+  --tensor-split 16/12/12  \
   --main-gpu 0 \
   --spec-type ngram-mod --spec-ngram-size-n 24 --draft-min 48 --draft-max 64 \
-  & disown
+  -p 2048 -n 2048 -b 1024 -ub 512 -ctk q4_0 --flash-attn
+#  & disown
+
 
