b5a06b5fae4f3fd5c09300216b90949a8d2fc309
jon welling <jon.welling@ipqs.com>
2026-03-01 08:33:07 -0500

    Added nv script, updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,7 @@
 
 cd ..
 mkdir models
+
 git clone https://github.com/ggml-org/llama.cpp
 export GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 
 export GGML_CCACHE=ON
@@ -13,6 +14,6 @@ nvidia-smi
 cd llama.cpp  
 git pull
 rm -r build/
-cmake -B build -DBUILD_SHARED_LIBS=OFF -DGGML_CUDA=ON -DGGML_NATIVE=OFF -DCMAKE_CUDA_ARCHITECTURES="86;120" -DGGML_CUDA_FORCE_MMQ=1 -DGGML_CUDA_PEER_MAX_BATCH_SIZE=256
+cmake -B build -DBUILD_SHARED_LIBS=OFF -DGGML_CUDA=ON -DGGML_NATIVE=OFF -DCMAKE_CUDA_ARCHITECTURES="86;89;120" -DGGML_CUDA_FORCE_MMQ=1 -DGGML_CUDA_PEER_MAX_BATCH_SIZE=256
 
 cmake --build build --config Release -j 6
