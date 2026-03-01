9c7811d8ac4bd3420d91622ccbc748189d2b8acb
jon welling <jon.welling@ipqs.com>
2026-03-01 17:45:24 -0500

    updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,7 @@ mkdir models
 sudo apt build-essential ccmake ccache nvidia-cuda-toolkit libopenblas-dev
 nvidia-smi
 
-rm -r llama.cpp
+sudo rm -r llama.cpp
 git clone https://github.com/ggml-org/llama.cpp
 # export GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 
 # export GGML_CCACHE=OFF
@@ -17,8 +17,6 @@ git clone https://github.com/ggml-org/llama.cpp
 nvidia-smi
 
 cd llama.cpp  
-git pull
-rm -r build/
 GGML_CCACHE=on cmake -B build -DBUILD_SHARED_LIBS=OFF -DGGML_CUDA=ON -DGGML_NATIVE=OFF -DCMAKE_CUDA_ARCHITECTURES=$cudav -DGGML_CUDA_FORCE_MMQ=1 -DGGML_CUDA_PEER_MAX_BATCH_SIZE=$cudaBatchSize
 
 GGML_CCACHE=on  cmake --build build --config Release -j 6
