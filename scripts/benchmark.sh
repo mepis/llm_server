81c4bde0ac2f357525d663b02967b7da5863835a
jon welling <jon.welling@ipqs.com>
2026-03-01 10:05:21 -0500

    updated benchmark script

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -6,6 +6,7 @@ batch=512-5120+512
 ubatch=512-5120+512
 flash=0
 mmapOn=1
+polling=10-100+10
 dioOn=1
 cachType=q4_0
 outputType=csv
@@ -17,4 +18,4 @@ echo ======================================
 echo "Enter model:"
 read model
 
-~/llm_server/llama.cpp/build/bin/./llama-bench -m ~/llm_server/models/$model --split-mode layer --tensor-split 16,12,12 --main-gpu 0  --batch-size $batch -ub $ubatch -ctk $cachType --flash-attn $flash --poll 75 --mmap $mmapOn --direct-io $dioOn -r $repeat # --progress --output $outputType
+~/llm_server/llama.cpp/build/bin/./llama-bench -m ~/llm_server/models/$model --split-mode layer --tensor-split 16,12,12 --main-gpu 0 --batch-size $batch -ub $ubatch -ctk $cachType --flash-attn $flash --poll $polling --mmap $mmapOn --direct-io $dioOn -r $repeat # --progress --output $outputType
