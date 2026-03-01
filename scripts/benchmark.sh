bd31fdaa8ef57f8556a3e575909f0990231c5acf
jon welling <jon.welling@ipqs.com>
2026-03-01 18:57:35 -0500

    updated scripts

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -10,6 +10,7 @@ polling=10-100+10
 dioOn=1
 cachType=q4_0
 outputType=csv
+ncmoe=0-30+1
 
 ls -l ~/llm_server/models *.gguf
 
@@ -18,4 +19,4 @@ echo ======================================
 echo "Enter model:"
 read model
 
-GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ~/llm_server/llama.cpp/build/bin/./llama-bench -m ~/llm_server/models/$model --split-mode layer --tensor-split 16,12,12 --main-gpu 0 --batch-size $batch -ub $ubatch -ctk $cachType --flash-attn $flash --poll $polling --mmap $mmapOn --direct-io $dioOn -r $repeat # --progress --output $outputType
+GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ~/llm_server/llama.cpp/build/bin/./llama-bench -m ~/llm_server/models/$model --split-mode layer --tensor-split 16,12,12 --main-gpu 0 --batch-size $batch -ub $ubatch -ctk $cachType --flash-attn $flash --poll $polling --mmap $mmapOn --direct-io $dioOn -r $repeat -ncmoe $ncmoe  #   --progress --output $outputType
