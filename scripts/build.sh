7f3e03c943a306197207dbf8e5d5d8d8a9fc54c8
jon welling <jon.welling@ipqs.com>
2026-03-01 17:49:43 -0500

    updated scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,19 +3,10 @@
 cudav="86;89"
 cudaBatchSize=64
 
-cd ..
-mkdir models
-
-sudo apt install build-essential cmake ccache nvidia-cuda-toolkit libopenblas-dev
-nvidia-smi
-
-sudo rm -r llama.cpp
-git clone https://github.com/ggml-org/llama.cpp
 # export GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 
 # export GGML_CCACHE=OFF
 
-nvidia-smi
-
+cd..
 cd llama.cpp  
 GGML_CCACHE=on cmake -B build -DBUILD_SHARED_LIBS=OFF -DGGML_CUDA=ON -DGGML_NATIVE=OFF -DCMAKE_CUDA_ARCHITECTURES=$cudav -DGGML_CUDA_FORCE_MMQ=1 -DGGML_CUDA_PEER_MAX_BATCH_SIZE=$cudaBatchSize
 
---
+++ scripts/init.sh
@@ -0,0 +1,8 @@
+cd ..
+mkdir models
+
+sudo apt install build-essential cmake ccache nvidia-cuda-toolkit libopenblas-dev
+nvidia-smi
+
+git clone https://github.com/ggml-org/llama.cpp
+nvidia-smi
