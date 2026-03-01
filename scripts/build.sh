48a7acf86cf99cdb0f27294c243a483e12a8666f
jon welling <jon.welling@ipqs.com>
2026-03-01 18:26:17 -0500

    updated scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,7 @@ export GGML_CCACHE=OFF
 
 cd ..
 cd llama.cpp  
+rm -r build
 cmake -B build -DBUILD_SHARED_LIBS=OFF -DGGML_CUDA=ON -DGGML_NATIVE=OFF -DCMAKE_CUDA_ARCHITECTURES=$cudav -DGGML_CUDA_FORCE_MMQ=1 -DGGML_CUDA_PEER_MAX_BATCH_SIZE=$cudaBatchSize
 
 cmake --build build --config Release -j 6
