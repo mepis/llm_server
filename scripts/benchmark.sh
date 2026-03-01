aa25c6472f5e956d9f44fd3691ff60c848f22fac
jon welling <jon.welling@ipqs.com>
2026-03-01 09:45:42 -0500

    benchmark update

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -12,4 +12,4 @@ read model
 
 # ~/llm_server/llama.cpp/build/bin/./llama-bench -m ~/llm_server/models/$model --split-mode layer --tensor-split 16,12,12 --main-gpu 0 -n 1024-5120+1024 --batch-size 4096 -ub 512 -ctk q4_0 --flash-attn 0 --poll 75 --mmap 1   --direct-io 1 --progress -r $repeat
 
-~/llm_server/llama.cpp/build/bin/./llama-bench -m ~/llm_server/models/$model --split-mode layer --tensor-split 16,12,12 --main-gpu 0 -n 1024-5120+1024 --batch-size 4096 -ub 512 -ctk q4_0 --flash-attn 0 --poll 75 --mmap 1   --direct-io 1 --progress -r $repeat
+~/llm_server/llama.cpp/build/bin/./llama-bench -m ~/llm_server/models/$model --split-mode layer --tensor-split 16,12,12 --main-gpu 0  --batch-size 4096 -ub 512 -ctk q4_0 --flash-attn 0 --poll 75 --mmap 1   --direct-io 1 --progress -r $repeat
