2ac50ca14aa7e66102443e669f3b252796b65be4
jon welling <jon.welling@ipqs.com>
2026-03-01 19:37:45 -0500

    updated scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ cudav="86;89"
 cudaBatchSize=64
 
 export GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 
-export GGML_CCACHE=OFF
+export GGML_CCACHE=ON
 
 # update nvcc path if needed
 # which nvcc
@@ -13,6 +13,6 @@ export GGML_CCACHE=OFF
 cd ..
 cd llama.cpp  
 rm -r build
-cmake -B build -DBUILD_SHARED_LIBS=OFF -DGGML_CUDA=ON -DGGML_NATIVE=OFF -DCMAKE_CUDA_ARCHITECTURES=$cudav -DGGML_CUDA_FORCE_MMQ=1 -DGGML_CUDA_PEER_MAX_BATCH_SIZE=$cudaBatchSize
+cmake -B build -DBUILD_SHARED_LIBS=OFF -DGGML_CUDA=ON -DGGML_NATIVE=OFF -DCMAKE_CUDA_ARCHITECTURES=$cudav -DGGML_CUDA_FORCE_MMQ=1 -DGGML_CUDA_PEER_MAX_BATCH_SIZE=$cudaBatchSize -DGGML_BLAS=ON -DGGML_BLAS_VENDOR=OpenBLAS
 
 cmake --build build --config Release -j 6
