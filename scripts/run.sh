68af4f2f4dcc275a11479985ba2a78bc0889ec1c
jon welling <jon.welling@ipqs.com>
2026-03-06 15:46:13 -0500

    run update

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,4 +11,4 @@ cd llama.cpp
 cd build
 cd bin
 
-GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ~/llm_server/llama.cpp/build/bin/./llama-server -m $model --port 3000 --host 100.115.205.84 -c 262144 -ngl 99 --split-mode layer --tensor-split 16,12,12 --main-gpu 0 -b 2048 -ub 1024 -ctk bf16 --threads 16 --temp 0.7 --repeat-penalty 1.0 --presence-penalty 1.5 --top-p 0.8 --min-p 0.0 --flash-attn 0 --webui --reasoning-budget 0 --parallel 4 --cont-batching
+GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ~/llm_server/llama.cpp/build/bin/./llama-server -m $model --port 3000 --host 100.115.205.84 -c 262144 -ngl 99 --split-mode layer --tensor-split 16,12,12 --main-gpu 0 -b 2048 -ub 1024 -ctk bf16 --threads 16 --temp 0.7 --repeat-penalty 1.0 --presence-penalty 1.5 --top-p 0.8 --min-p 0.0 --flash-attn 0 --webui --reasoning-budget 0 --parallel 8 --cont-batching --kv-unified
