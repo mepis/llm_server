72b7b4b9fe55ee8976de780b2eb7385596810caf
jon welling <jon.welling@ipqs.com>
2026-03-06 15:35:45 -0500

    run update

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,8 @@
 
 # model=AesSedai/Qwen3.5-35B-A3B-GGUF:Q4_K_M
 # model=unsloth/Qwen3.5-27B-GGUF:Q4_K_S
-model=home/jon/llm_server/models/unsloth/Qwen3.5-35B-A3B-GGUF:UD-Q8_K_XL
+# model=unsloth/Qwen3.5-35B-A3B-GGUF:UD-Q8_K_XL
+model=/home/jon/llm_server/models/unsloth_Qwen3.5-35B-A3B-GGUF_Qwen3.5-35B-A3B-UD-Q4_K_XL.gguf
 
 export LLAMA_CACHE=/home/jon/llm_server/models
 cd ..
@@ -10,4 +11,4 @@ cd llama.cpp
 cd build
 cd bin
 
-GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ~/llm_server/llama.cpp/build/bin/./llama-server -hf $model --port 3000 --host 100.115.205.84 -c 262144 -ngl 99 --split-mode layer --tensor-split 16,12,12 --main-gpu 0 -b 2048 -ub 1024 -ctk bf16 --threads 16 --temp 0.7 --repeat-penalty 1.0 --presence-penalty 1.5 --top-p 0.8 --min-p 0.0 --flash-attn 0 --webui --reasoning-budget 0 --parallel 4 --cont-batching
+GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ~/llm_server/llama.cpp/build/bin/./llama-server -m $model --port 3000 --host 100.115.205.84 -c 262144 -ngl 99 --split-mode layer --tensor-split 16,12,12 --main-gpu 0 -b 2048 -ub 1024 -ctk bf16 --threads 16 --temp 0.7 --repeat-penalty 1.0 --presence-penalty 1.5 --top-p 0.8 --min-p 0.0 --flash-attn 0 --webui --reasoning-budget 0 --parallel 4 --cont-batching
