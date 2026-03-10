46b938e66c52a067484b1f86bfebaf3a8bf6c20f
jon welling <jon.welling@ipqs.com>
2026-03-09 22:05:34 -0400

    maint commit

--- scripts/run.sh
+++ scripts/run.sh
@@ -16,7 +16,7 @@ cd build
 cd bin
 
 # Multi GPU (3x - 16gb, 12gb, 12gb) w/ already downloaded model
-~/llm_server/llama.cpp/build/bin/./llama-server -m $model --port 3000 --host 100.115.205.84 -c 131072 -ngl 99 --split-mode layer --tensor-split 16,12,12 --main-gpu 0 -b 2048 -ub 1024 -ctk bf16 --threads 16 --temp 0.7 --repeat-penalty 1.0 --presence-penalty 1.5 --top-p 0.8 --min-p 0.0 --flash-attn 0 --webui --reasoning-budget 0 --parallel 4 --cont-batching --kv-unified
+~/llm_server/llama.cpp/build/bin/./llama-server -m $model --port 3000 --host 100.115.205.84 -c 131072 -ngl 99 --split-mode layer --tensor-split 16,12,12 --main-gpu 0 -b 2048 -ub 1024 -ctk bf16 --threads 20 --temp 0.7 --repeat-penalty 1.0 --presence-penalty 1.5 --top-p 0.8 --min-p 0.0 --flash-attn 0 --webui --reasoning-budget 0 --parallel 4 --cont-batching --kv-unified
 
 # Single GPU w/ higgingface import
 # ~/git/llm_server/llama.cpp/build/bin/./llama-server -hf $model --port 3000 --host 127.0.0.1 -c 32768 -ngl 99 -b 1024 -ub 512 -ctk q8_0 --threads $threads --temp 0.7 --repeat-penalty 1.0 --presence-penalty 1.5 --top-p 0.8 --min-p 0.0 --flash-attn 0 --webui --reasoning-budget 0 --cont-batching --cache-ram 0 # --kv-unified 
