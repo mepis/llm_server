c58531c7875c6ade460f185be620b9a47095df22
jon welling <jon.welling@ipqs.com>
2026-03-10 09:21:02 -0400

    maint commit

--- scripts/run.sh
+++ scripts/run.sh
@@ -20,3 +20,6 @@ GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ~/llm_server/llama.cpp/build/bin/./llama-serve
 
 # Single GPU w/ higgingface import
 # ~/git/llm_server/llama.cpp/build/bin/./llama-server -hf $model --port 3000 --host 127.0.0.1 -c 32768 -ngl 99 -b 1024 -ub 512 -ctk q8_0 --threads $threads --temp 0.7 --repeat-penalty 1.0 --presence-penalty 1.5 --top-p 0.8 --min-p 0.0 --flash-attn 0 --webui --reasoning-budget 0 --cont-batching --cache-ram 0 # --kv-unified 
+
+# local laptop
+# llama-server -hf unsloth/Qwen3.5-9B-GGUF:UD-Q4_K_XL --port 3000 --host 100.110.63.5 -b 2048 -ub 1024 -ctk bf16 --threads 28 --temp 0.7 --repeat-penalty 1.0 --presence-penalty 1.5 --top-p 0.8 --min-p 0.0 --flash-attn 0 --webui --reasoning-budget 0 --parallel 2 --cont-batching --kv-unified
