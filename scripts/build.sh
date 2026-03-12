4a9abfc08a66ee3373a6cdf2b2ea048e71f20944
jon welling <jon.welling@ipqs.com>
2026-03-12 19:45:55 -0400

    maint update

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,10 +3,9 @@
 cudav="86;89"
 cudaBatchSize=64
 
-cd ~/git/llm_server/
+cd ~/llm_server/
 git clone https://github.com/ggml-org/llama.cpp
 
-export GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 
 export GGML_CCACHE=ON
 
 # update nvcc path if needed
@@ -14,7 +13,7 @@ export GGML_CCACHE=ON
 # export CUDACXX=/usr/bin/nvcc
 
 cd ..
-cd ~/git/llm_server/llama.cpp  
+cd ~/llm_server/llama.cpp  
 rm -r build
 cmake -B build -DBUILD_SHARED_LIBS=OFF -DGGML_CUDA=ON -DGGML_NATIVE=OFF -DCMAKE_CUDA_ARCHITECTURES=$cudav -DGGML_CUDA_FORCE_MMQ=1 -DGGML_CUDA_PEER_MAX_BATCH_SIZE=$cudaBatchSize -DGGML_BLAS=ON -DGGML_BLAS_VENDOR=OpenBLAS
 
