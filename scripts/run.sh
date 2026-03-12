d7d8a6bf3062d6c1a402c93a8edf7e23d16e1b47
jon welling <jon.welling@ipqs.com>
2026-03-12 15:27:37 -0400

    maint update

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,5 +12,5 @@ cd bin
 
 export LLAMA_CACHE=/home/jon/git/llm_server/models
 
-GGML_CUDA_FORCE_MMQ=true GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ~/llm_server/llama.cpp/build/bin/./llama-server -hf $model --port 3000 --host 100.115.205.84 -c 65536 -ngl 99 --split-mode layer --tensor-split 16,12,12 --main-gpu 0 -b 2048 -ub 1024 -ctk q8_0 --threads $threads --temp 0.7 --repeat-penalty 1.0 --presence-penalty 1.5 --top-p 0.8 --min-p 0.0 --flash-attn 0 --webui --reasoning-budget 0 --cont-batching --kv-unified --log-file $logDir --verbose
+GGML_CUDA_FORCE_MMQ=true GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ~/llm_server/llama.cpp/build/bin/./llama-server -hf $model --port 3000 --host 100.115.205.84 -c 65536 -ngl 99 --split-mode layer --tensor-split 16,12,12 --main-gpu 0 -b 2048 -ub 1024 -ctk bf16 --threads $threads --temp 0.7 --repeat-penalty 1.0 --presence-penalty 1.5 --top-p 0.8 --min-p 0.0 --flash-attn 0 --webui --reasoning-budget 0 --cont-batching --kv-unified --log-file $logDir --verbose
 
