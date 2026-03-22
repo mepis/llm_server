53c77f729f14a68e4d182c25a8ed77f70501bd13
jon welling <jon.welling@ipqs.com>
2026-03-22 09:58:01 -0400

    maint update

--- scripts/run_ik.sh
+++ scripts/run_ik.sh
@@ -20,7 +20,6 @@ threads=16
 context=262144
 temp=0.6
 topP=0.95
-prio=3
 minP=0.00
 topK=0.95
 
@@ -32,4 +31,4 @@ logsDir="${LLM_SERVER_HOME}logs"
 cd "${LLM_SERVER_HOME}"
 cd ik_llama.cpp/build/bin/
 
-LLAMA_CACHE=$modelDir GGML_CUDA_FORCE_MMQ=true GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ./llama-server -m "${MODELS_DIR}${model}" --port $port --host $host -c $context -ngl 99 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching --prio $prio --min_p $minP --threads $threads --top-k $topK --kv-unified --log-file $logDir -np 4 -pps
+LLAMA_CACHE=$modelDir GGML_CUDA_FORCE_MMQ=true GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ./llama-server -m "${MODELS_DIR}${model}" --port $port --host $host -c $context -ngl 99 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching --min_p $minP --threads $threads --top-k $topK --kv-unified --log-file $logDir -np 4 -pps
