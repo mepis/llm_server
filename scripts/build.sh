79dc1a758366b22e2c391982711cb8ebf056ede9
jon welling <jon.welling@ipqs.com>
2026-03-22 16:56:25 -0400

    maint update

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,6 +15,7 @@ export GGML_CCACHE=ON
 cd $LLM_SERVER_HOME
 git clone https://github.com/ggml-org/llama.cpp 
 cd llama.cpp
+git pull
 rm -r build
 cmake -B build -DBUILD_SHARED_LIBS=OFF -DGGML_CUDA=ON -DGGML_NATIVE=OFF -DCMAKE_CUDA_ARCHITECTURES=$cudav -DGGML_CUDA_FORCE_MMQ=1 -DGGML_CUDA_PEER_MAX_BATCH_SIZE=$cudaBatchSize -DGGML_BLAS=ON -DGGML_BLAS_VENDOR=OpenBLAS
 cmake --build build --config Release -j 6
