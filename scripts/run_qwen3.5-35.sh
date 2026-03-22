854726a0fd581892000992d7fc4b18100bd1b3db
jon welling <jon.welling@ipqs.com>
2026-03-22 15:03:46 -0400

    maint commit

--- scripts/run_qwen3.5-35.sh
+++ scripts/run_qwen3.5-35.sh
@@ -27,6 +27,8 @@ topK=0.95
 modelDir=/home/jon/.llm_server/models/
 
 cd "${LLM_SERVER_HOME}"
+git pull
+
 cd llama.cpp/build/bin/
 
 LLAMA_CACHE=$modelDir GGML_CUDA_FORCE_MMQ=true GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ./llama-server -hf unsloth/Qwen3.5-35B-A3B-GGUF:Q8_0 --port $port --host $host -c $context -ngl 99 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching  --min_p $minP --threads $threads --top-k $topK -ctk q8_0 --kv-unified -np 8 -b 1024 --swa-full --dio --prio $threatPriority
