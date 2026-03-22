73ee15397c7d145093e020871cf3ee2dde6056d8
jon welling <jon.welling@ipqs.com>
2026-03-22 10:21:55 -0400

    maint update

--- scripts/run_qwen3.5-35.sh
+++ scripts/run_qwen3.5-35.sh
@@ -12,6 +12,7 @@ mainGpu=0
 tensorSplit=16,12,12
 splitMode=row
 threads=26
+threatPriority=2
 
 ###############
 # Model Configs
@@ -28,4 +29,4 @@ modelDir=/home/jon/.llm_server/models/
 cd "${LLM_SERVER_HOME}"
 cd llama.cpp/build/bin/
 
-LLAMA_CACHE=$modelDir GGML_CUDA_FORCE_MMQ=true GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ./llama-server -hf unsloth/Qwen3.5-35B-A3B-GGUF:Q8_0 --port $port --host $host -c $context -ngl 99 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching  --min_p $minP --threads $threads --top-k $topK -ctk q8_0 --kv-unified -np 8 -b 1024 --swa-full -dio --mmap
+LLAMA_CACHE=$modelDir GGML_CUDA_FORCE_MMQ=true GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ./llama-server -hf unsloth/Qwen3.5-35B-A3B-GGUF:Q8_0 --port $port --host $host -c $context -ngl 99 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching  --min_p $minP --threads $threads --top-k $topK -ctk q8_0 --kv-unified -np 8 -b 1024 --swa-full -dio --mmap --prio $threatPriority
