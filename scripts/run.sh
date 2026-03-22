b02a51889e7ae638e262b3e01bac7dcc19fe00d0
jon welling <jon.welling@ipqs.com>
2026-03-22 13:54:44 -0400

    maint update

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,6 +12,7 @@ mainGpu=0
 tensorSplit=16,12,12
 splitMode=layer
 threads=16
+threatPriority=2
 
 ###############
 # Model Configs
@@ -49,4 +50,4 @@ cd llama.cpp/build/bin/
 
 
 
-LLAMA_CACHE=$modelDir GGML_CUDA_FORCE_MMQ=true GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ./llama-server -m "${MODELS_DIR}${model}" --port $port --host $host -c $context -ngl 99 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching  --min_p $minP --threads $threads --top-k $topK --kv-unified --log-file $logDir -np 4 -pps
+LLAMA_CACHE=$modelDir GGML_CUDA_FORCE_MMQ=true GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ./llama-server -m "${MODELS_DIR}${model}" --port $port --host $host -c $context -ngl 99 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching  --min_p $minP --threads $threads --top-k $topK --kv-unified --log-file $logDir --kv-unified -np 8 -b 1024 --swa-full -dio --mmap --prio $threatPriority
