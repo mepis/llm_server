76d3c4d5c9849f8ebaf837b3e8d42322f5ce1f0d
jon welling <jon.welling@ipqs.com>
2026-03-22 16:20:48 -0400

    maint commit

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,8 +10,8 @@ host=100.115.205.84
 # Hardware Configs
 mainGpu=0
 tensorSplit=16,12,12
-splitMode=graph
-threads=16
+splitMode=row
+threads=26
 threadPriority=3
 
 ###############
@@ -50,4 +50,4 @@ cd llama.cpp/build/bin/
 
 
 
-LLAMA_CACHE=$modelDir GGML_CUDA_FORCE_MMQ=true GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ./llama-server -m /home/jon/.llm_server/models/Qwen3.5-35B-A3B-UD-Q6_K_S.gguf --port $port --host $host -c $context -ngl 99 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching --min-p $minP --threads $threads --top-k $topK -np 16 -b 1024 --prio $threadPriority
+LLAMA_CACHE=$modelDir GGML_CUDA_FORCE_MMQ=true GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 ./llama-server -m /home/jon/.llm_server/models/Qwen3.5-35B-A3B-UD-Q6_K_S.gguf --port $port --host $host -c $context -ngl 99 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching --min-p $minP --threads $threads --top-k $topK -np 16 --prio $threadPriority -b 2048 -ub 1024 -ctk q8_0 --kv-unified 
