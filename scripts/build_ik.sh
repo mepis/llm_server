0f1d3c17b9431519943e2cc374d5823c8fb3cfe6
jon welling <jon.welling@ipqs.com>
2026-03-24 20:15:41 -0400

    maint commit

---
+++ scripts/build_ik.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+LLM_SERVER_HOME=~/.llm_server
+
+cudav="86;89"
+cudaBatchSize=1024
+
+
+# update nvcc path if needed
+# which nvcc
+# export CUDACXX=/usr/bin/nvcc
+
+export GGML_CCACHE=ON
+
+cd $LLM_SERVER_HOME
+git clone https://github.com/ikawrakow/ik_llama.cpp.git 
+cd ik_llama.cpp
+git pull
+rm -r build
+cmake -B build -DBUILD_SHARED_LIBS=OFF -DGGML_CUDA=ON -DGGML_NATIVE=OFF -DCMAKE_CUDA_ARCHITECTURES=$cudav -DGGML_CUDA_FORCE_MMQ=1 -DGGML_CUDA_PEER_MAX_BATCH_SIZE=$cudaBatchSize -DGGML_BLAS=ON -DGGML_BLAS_VENDOR=OpenBLAS
+cmake --build build --config Release -j 6
