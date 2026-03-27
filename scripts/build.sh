99946462bb11a08145d07b3240aa46ad92bbf028
jon welling <jon.welling@ipqs.com>
2026-03-27 09:30:35 -0400

    maint commit

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 LLM_SERVER_HOME=~/.llm_server
 
 cudav="86;89"
-cudaBatchSize=1024
+cudaBatchSize=512
 
 
 # update nvcc path if needed
@@ -17,5 +17,8 @@ git clone https://github.com/ggml-org/llama.cpp
 cd llama.cpp
 git pull
 rm -r build
-cmake -B build -DBUILD_SHARED_LIBS=OFF -DGGML_CUDA=ON -DGGML_NATIVE=OFF -DCMAKE_CUDA_ARCHITECTURES=$cudav -DGGML_CUDA_FORCE_MMQ=1 -DGGML_CUDA_PEER_MAX_BATCH_SIZE=$cudaBatchSize -DGGML_BLAS=ON -DGGML_BLAS_VENDOR=OpenBLAS
-cmake --build build --config Release -j 6
+cmake -B build -DBUILD_SHARED_LIBS=OFF -DGGML_CUDA=ON -DGGML_CUDA_PEER_MAX_BATCH_SIZE=$cudaBatchSize -DGGML_BLAS=ON -DGGML_BLAS_VENDOR=OpenBLAS
+cmake --build build --config Release -j 10
+
+# -DCMAKE_CUDA_ARCHITECTURES=$cudav 
+# -DGGML_NATIVE=OFF 
