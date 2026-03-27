abf82dee694a56de3b6de275db163922d6e6ed5a
jon welling <jon.welling@ipqs.com>
2026-03-27 09:24:16 -0400

    maint commit

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+LLM_SERVER_HOME=/home/jon/llm_server/scripts/
+cd "${LLM_SERVER_HOME}"
+
 systemctl --user stop llama.service
 git pull
+
 ./buid.sh
 systemctl --user start llama.service
 
