beffc2c9d6d1c13941f95cda99878b10db49c0bc
jon welling <jon.welling@ipqs.com>
2026-03-28 12:33:02 -0400

    maint commit

--- scripts/run_nvidia_Nemotron-Cascade-2-30B-A3B-Q6_K_L.sh
+++ scripts/run_nvidia_Nemotron-Cascade-2-30B-A3B-Q6_K_L.sh
@@ -32,4 +32,4 @@ export LLAMA_CACHE=$modelDir
 export GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 
 export CUDACXX=/usr/local/cuda/bin/nvcc
 
-taskset -c 0-11 ./llama-server -m /home/jon/.llm_server/models/nvidia_Nemotron-Cascade-2-30B-A3B-Q6_K_L.gguf --port $port --host $host -c $context -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching --min-p $minP --top-k $topK -ctk q8_0 --threads $threads --prio 3 --jinja
+taskset -c 0-11 ./llama-server -m /home/jon/.llm_server/models/nvidia_Nemotron-Cascade-2-30B-A3B-Q6_K_L.gguf --port $port --host $host -c $context -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching --min-p $minP --top-k $topK -ctk q8_0 --threads $threads 
