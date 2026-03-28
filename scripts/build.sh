fa8c475dfe28c6b69d0fe40a9a0eeded9f8bd461
jon welling <jon.welling@ipqs.com>
2026-03-28 11:09:54 -0400

    maint commit

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,30 +16,11 @@ cd llama.cpp
 git pull
 rm -r build
 
-# LLAMA OPTIONS
-export GGML_CCACHE=OFF
-export GGML_LTO=on
-export GGML_RPC=on
-export BUILD_SHARED_LIBS=on
-export GGML_STATIC=on
-
-# CPU OPTIONS
-export GGML_CPU=off
-
-# BLAS OPTIONS
-export GGML_BLAS=off
-# export GGML_BLAS_VENDOR=OpenBLAS 
-
 # CUDA OPTIONS
 # update nvcc path if needed
 # which nvcc
 export CUDACXX=/usr/local/cuda/bin/nvcc
-export GGML_CUDA=on
-export GGML_CUDA_PEER_MAX_BATCH_SIZE=256
-export GGML_CUDA_COMPRESSION_MODE=balance
-export GGML_CUDA_FORCE_MMQ=on
-export GGML_CUDA_GRAPHS=on
-export GGML_NATIVE=on
 
-cmake -B build  
+
+cmake -B build -DGGML_CCACHE=OFF -DGGML_LTO=on -DGGML_RPC=on -DBUILD_SHARED_LIBS=on -DGGML_STATIC=on -DGGML_CUDA=on -DGGML_CUDA_PEER_MAX_BATCH_SIZE=256 -DGGML_CUDA_COMPRESSION_MODE=balance -DGGML_CUDA_FORCE_MMQ=on -DGGML_CUDA_GRAPHS=on -DGGML_NATIVE=on -DGGML_BLAS=off -DGGML_CPU=off
 cmake --build build --config Release -j "${nproc}"
