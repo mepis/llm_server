f64deab20086816cb66b15ac0706decfa4d2bb3f
jon welling <jon.welling@ipqs.com>
2026-03-28 14:21:46 -0400

    maint commit

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,6 @@
 LLM_SERVER_HOME=/home/jon/llm_server/scripts/
 cd "${LLM_SERVER_HOME}"
 
-./run_qwen3.5-35-q8.sh
-# ./run_Nemotron-3-Nano-30B-A3B-IQ4_NL.sh
+# ./run_qwen3.5-35-q8.sh
+./run_Nemotron-3-Nano-30B-A3B-IQ4_NL.sh
 # ./run_nvidia_Nemotron-Cascade-2-30B-A3B-Q6_K_L.sh
