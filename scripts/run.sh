20d19ee76997eefbaed76290b74669da753ac7f7
jon welling <jon.welling@ipqs.com>
2026-04-02 21:55:00 -0400

    maint commit

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,9 +4,9 @@
 # model=run_Nemotron-3-Nano-30B-A3B-IQ4_NL.sh
 # model=run_nvidia_Nemotron-Cascade-2-30B-A3B-Q6_K_L.sh
 # model=run_Qwen3.5-27B-IQ4_NL.sh
-model=run_Qwen3-Coder-Next-MXFP4_MOE.sh
+# model=run_Qwen3-Coder-Next-MXFP4_MOE.sh
 # model=run_Qwen3.5-9B-IQ4_NL.sh
-# model=run_gemma-4-26B-A4B-it-MXFP4_MOE.sh
+model=run_gemma-4-26B-A4B-it-MXFP4_MOE.sh
 
 CURRENT_DIR=$(pwd)
 cd $CURRENT_DIR
