1941c33bc858c70752a0da4625ed5ff64ac5f3af
jon welling <jon.welling@ipqs.com>
2026-04-10 19:25:21 -0400

    maint commit

--- scripts/update_llama.sh
+++ scripts/update_llama.sh
@@ -16,7 +16,7 @@ cmake -B build -DGGML_CCACHE=on -DGGML_LTO=on -DGGML_CUDA=on  -DGGML_CUDA_GRAPHS
 # -DGGML_CUDA_FORCE_MMQ=on -DGGML_CUDA_PEER_MAX_BATCH_SIZE=512 -DGGML_CPU=off -DGGML_CUDA_COMPRESSION_MODE=off -DGGML_CUDA_FA_ALL_QUANTS=on -DGGML_CUDA_FORCE_CUBLAS=on
 
 # Add -j "${nproc}" or -j 4 parameters to make compile faster with risk of running out of memory
-cmake --build build --config Release -j 10 --clean-first  
+cmake --build build --config Release -j 4 --clean-first  
 
 systemctl --user stop llama.service
 systemctl --user start llama.service
