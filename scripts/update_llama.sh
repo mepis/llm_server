8eea1240fdbf82420725c70d70dea3a28a252c0a
jon welling <jon.welling@ipqs.com>
2026-04-10 19:22:45 -0400

    maint commit

--- scripts/update_llama.sh
+++ scripts/update_llama.sh
@@ -11,9 +11,9 @@ git pull
 NVCC_PATH=$(which nvcc) 
 export CUDACXX=$NVCC_PATH
 
-cmake -B build -DGGML_CCACHE=on -DGGML_LTO=on -DGGML_CUDA=on  -DGGML_CUDA_GRAPHS=on -DGGML_CUDA_FA=on -DGGML_CUDA_FORCE_CUBLAS=on
+cmake -B build -DGGML_CCACHE=on -DGGML_LTO=on -DGGML_CUDA=on  -DGGML_CUDA_GRAPHS=on -DGGML_CUDA_FA=on 
 
-# -DGGML_CUDA_FORCE_MMQ=on -DGGML_CUDA_PEER_MAX_BATCH_SIZE=512 -DGGML_CPU=off -DGGML_CUDA_COMPRESSION_MODE=off -DGGML_CUDA_FA_ALL_QUANTS=on 
+# -DGGML_CUDA_FORCE_MMQ=on -DGGML_CUDA_PEER_MAX_BATCH_SIZE=512 -DGGML_CPU=off -DGGML_CUDA_COMPRESSION_MODE=off -DGGML_CUDA_FA_ALL_QUANTS=on -DGGML_CUDA_FORCE_CUBLAS=on
 
 # Add -j "${nproc}" or -j 4 parameters to make compile faster with risk of running out of memory
 cmake --build build --config Release -j 10 --clean-first  
