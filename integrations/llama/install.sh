62f4d2052c28cde410dde708f6f5fb0d04a16903
jon welling <jon.welling@ipqs.com>
2026-04-10 21:17:42 -0400

    maint commit

--- integrations/llama/install.sh
+++ integrations/llama/install.sh
@@ -1,17 +1,20 @@
-git clone https://github.com/ggml-org/llama.cpp  
 
-cd llama.cpp
+git clone https://github.com/ggml-org/llama.cpp  $HOME/.llama.cpp
+
+cd $HOME
+cd .llama.cpp
+rm -r build
 git pull
 
 NVCC_PATH= which nvcc 
 export CUDACXX=$NVCC_PATH
 
-CUDACXX=$NVCC_PATH cmake -B build -DGGML_LTO=on -DGGML_CUDA=on -DGGML_CUDA_GRAPHS=on -DGGML_CUDA_FA=on -DGGML_CUDA_PEER_MAX_BATCH_SIZE=512 -DGGML_CUDA_FA_ALL_QUANTS=on -DGGML_CUDA_COMPRESSION_MODE=none -DGGML_CUDA_FORCE_CUBLAS=on 
+cmake -B build -DGGML_LTO=on -DGGML_CUDA=on -DGGML_CUDA_GRAPHS=on -DGGML_CUDA_FA=on -DGGML_CUDA_PEER_MAX_BATCH_SIZE=512 -DGGML_CUDA_FA_ALL_QUANTS=on -DGGML_CUDA_COMPRESSION_MODE=none -DGGML_CUDA_FORCE_CUBLAS=on 
 
 # -DGGML_CUDA_FORCE_MMQ=on -DGGML_CUDA_PEER_MAX_BATCH_SIZE=512 -DGGML_CPU=off -DGGML_CUDA_COMPRESSION_MODE=off -DGGML_CUDA_FA_ALL_QUANTS=on -DGGML_CUDA_FORCE_CUBLAS=on -DGGML_CCACHE=on 
 
 # Add -j "${nproc}" or -j 4 parameters to make compile faster with risk of running out of memory
-CUDACXX=$NVCC_PATH cmake --build build --config Release -j 8 --clean-first  
+cmake --build build --config Release -j 8 --clean-first  
 
 systemctl --user stop llama.service
 systemctl --user start llama.service
