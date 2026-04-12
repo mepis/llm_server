17ba73192eaeba85c5e771ba4a4893b41902dc4d
jon welling <jon.welling@ipqs.com>
2026-04-11 22:03:44 -0400

    maint commit

--- integrations/llama/install.sh
+++ integrations/llama/install.sh
@@ -2,6 +2,20 @@
 
 CURRENT_DIR=$(pwd)
 
+echo -e "
+#!/bin/bash
+
+# model=Qwen3.5-27B-IQ4_NL.sh
+model=gemma-4-26B-A4B-it-MXFP4_MOE.sh
+# model=Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-Q4_K_M.sh
+
+CURRENT_DIR=$(pwd)
+cd $CURRENT_DIR
+
+$CURRENT_DIR/models/./$model
+
+" >> $CURRENT_DIR/run.sh
+
 git clone https://github.com/ggml-org/llama.cpp 
 
 cd llama.cpp
@@ -38,19 +52,8 @@ WantedBy=default.target
 " >> $HOME/.config/systemd/user/llama.service
 
 
-echo -e "
-#!/bin/bash
+cd ..
 
-# model=Qwen3.5-27B-IQ4_NL.sh
-model=gemma-4-26B-A4B-it-MXFP4_MOE.sh
-# model=Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-Q4_K_M.sh
-
-CURRENT_DIR=$(pwd)
-cd $CURRENT_DIR
-
-$CURRENT_DIR/models/./$model
-
-" >> $CURRENT_DIR/run.sh
 chmod 755 run.sh
 
 loginctl enable-linger $USER
