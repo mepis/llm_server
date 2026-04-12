50689f93f5210d06caf50c1559aa3f6b25d4a728
jon welling <jon.welling@ipqs.com>
2026-04-12 10:56:57 -0400

    maint commit

--- integrations/init_ubuntu.sh
+++ integrations/init_ubuntu.sh
@@ -1,12 +1,6 @@
-cd /tmp
-wget -q https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb 
-dpkg -i cuda-keyring_1.1-1_all.deb 
-
 # Install pre-reqs
 sudo apt update 
 sudo apt upgrade -y 
 
 sudo apt install git build-essential cmake ccache pkg-config libssl-dev libnccl-dev libcurl4-openssl-dev curl libgomp1 software-properties-common clinfo ninja-build nvidia-cuda-toolkit -y 
 
-sudo apt update 
-sudo apt upgrade -y 
