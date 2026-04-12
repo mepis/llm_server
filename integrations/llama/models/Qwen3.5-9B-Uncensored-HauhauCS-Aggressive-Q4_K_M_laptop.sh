18a8bd1d911b6ab4ea176af5f0f72c8b586cab7f
jon welling <jon.welling@ipqs.com>
2026-04-12 12:38:57 -0400

    maint commit

--- integrations/llama/models/Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-Q4_K_M_laptop.sh
+++ integrations/llama/models/Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-Q4_K_M_laptop.sh
@@ -3,11 +3,11 @@ model=Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-Q4_K_M.gguf
 
 # Host Configs
 port=11434
-host=100.115.205.84
+host=100.120.132.78
 
 # Hardware Configs
 mainGpu=0
-tensorSplit=16,12,12
+tensorSplit=8
 splitMode=layer
 threads=8
 
@@ -28,4 +28,4 @@ export LLAMA_CACHE=$modelDir
 export GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 
 export CUDACXX=$(which nvcc)
 
-./llama-server -m $model --mmproj $model --port $port --host $host -c $context -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching --min-p $minP --top-k $topK --threads $threads --prio 3 --cpu-range 0-7 --cpu-strict 1 --swa-full --kv-unified --cache-type-k q8_0 --cache-type-v q8_0 --batch-size 4096 --ubatch-size 1024 --chat-template-kwargs '{"enable_thinking":true}' --presence-penalty 1 --repeat-penalty 1
+./llama-server -m $CURRENT_DIR/models/$model --port $port --host $host -c $context -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching --min-p $minP --top-k $topK --threads $threads --prio 3 --cpu-range 0-7 --cpu-strict 1 --swa-full --kv-unified --cache-type-k q8_0 --cache-type-v q8_0 --batch-size 4096 --ubatch-size 1024 --chat-template-kwargs '{"enable_thinking":true}' --presence-penalty 1 --repeat-penalty 1
