456e601671daa2bc6c7eb99be4254f862cb5250c
jon welling <jon.welling@ipqs.com>
2026-04-19 11:18:40 -0400

    benchmark script update

--- integrations/llama/benchmark.sh
+++ integrations/llama/benchmark.sh
@@ -36,6 +36,6 @@ export LLAMA_ARG_FIT=on
 export LLAMA_ARG_FIT_TARGET=256
 export LLAMA_ARG_FIT_CTX=262144
 
-./llama-bench -m $MODEL_DIR/$model -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --threads $threads --prio 2 --cpu-strict 1  --cache-type-k q8_0 --cache-type-v q8_0 --batch-size $batch_size --ubatch-size $ubatch_size 
+./llama-bench -m $MODEL_DIR/$model -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --threads $threads --cpu-strict 1  --cache-type-k q8_0 --cache-type-v q8_0 --batch-size $batch_size --ubatch-size $ubatch_size 
 
 # --mmap $mmap --fit $fit 
