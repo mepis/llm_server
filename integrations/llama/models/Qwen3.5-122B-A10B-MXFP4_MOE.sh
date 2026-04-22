ada4640a4d9e225603701d888a9b364e4f2237ab
jon welling <jon.welling@ipqs.com>
2026-04-22 07:56:43 -0400

    maint commit

--- integrations/llama/models/Qwen3.5-122B-A10B-MXFP4_MOE.sh
+++ integrations/llama/models/Qwen3.5-122B-A10B-MXFP4_MOE.sh
@@ -29,4 +29,4 @@ export LLAMA_CACHE=$modelDir
 export GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 
 export CUDACXX=$(which nvcc)
 
-./llama-server -m $MODEL_DIR/$model --port $port --host $host -c $context -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching --min-p $minP --top-k $topK --threads $threads --prio 2 --cpu-range 0-7 --cpu-strict 1 --swa-full --kv-unified --cache-type-k q8_0 --cache-type-v q8_0 --batch-size 512 --ubatch-size 256 --chat-template-kwargs '{"enable_thinking":true}' --presence-penalty 1 --repeat-penalty 1
+./llama-server -m $MODEL_DIR/$model --port $port --host $host -c $context -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching --min-p $minP --top-k $topK --threads $threads --prio 2 --cpu-range 0-7 --cpu-strict 1 --kv-unified --cache-type-k q4_0 --cache-type-v q4_0 --batch-size 512 --ubatch-size 256 --chat-template-kwargs '{"enable_thinking":true}' --presence-penalty 1.0 --repeat-penalty 1.0
