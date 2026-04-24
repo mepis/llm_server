89521462ac17424ba09a5635c3bd7ef3f70fc072
jon welling <jon.welling@ipqs.com>
2026-04-24 11:13:34 -0400

    Updated llama install.sh script

--- integrations/llama/install.sh
+++ integrations/llama/install.sh
@@ -3,7 +3,7 @@ CURRENT_DIR=$(pwd)
 
 ## GEMMA ##
 ##--------
-MODEL=gemma-4-26B-A4B.sh
+# MODEL=gemma-4-26B-A4B.sh
 
 ## NEMOTRON ##
 ##--------
@@ -11,13 +11,13 @@ MODEL=gemma-4-26B-A4B.sh
 
 ## QWEN ##
 ##--------
-# MODEL=Qwen3.6-35B-A3B-MXFP4_MOE.sh
 # MODEL=Qwen3.5-122B-A10B-UD-IQ2_XXS.sh
 # MODEL=Qwen3.5-122B-A10B-MXFP4_MOE.sh
 # MODEL=Qwen3.5-35B-A3B-MXFP4_MOE.sh
 # MODEL=Qwen3.5-27B-IQ4_NL.sh
 # MODEL=Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-Q4_K_M.sh
-# MODEL=Qwen3.6-27B-Q8_0.sh
+# MODEL=Qwen3.6-35B-A3B-MXFP4_MOE.sh
+MODEL=Qwen3.6-27B-Q8_0.sh
 
 ###############################################################
 
