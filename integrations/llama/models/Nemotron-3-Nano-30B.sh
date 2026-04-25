0f18b3fe3d8d43354b3da616727c3337ab7be629
jon welling <jon.welling@ipqs.com>
2026-04-24 22:39:41 -0400

    Updated llama install.sh script

--- integrations/llama/models/Nemotron-3-Nano-30B.sh
+++ integrations/llama/models/Nemotron-3-Nano-30B.sh
@@ -7,7 +7,7 @@ host=100.106.131.63
 
 # Hardware Configs
 mainGpu=0
-tensorSplit=16,12,12
+tensorSplit=8
 splitMode=layer
 threads=20
 
@@ -29,6 +29,6 @@ export LLAMA_CACHE=$modelDir
 export GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 
 export CUDACXX=$(which nvcc)
 
-./llama-server -m $MODEL_DIR/$model --port $port --host $host -c $context -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP  --min-p $minP --top-k $topK --threads $threads --swa-full --kv-unified --cache-type-k q8_0 --cache-type-v q8_0 --batch-size 512 --ubatch-size 256  --presence-penalty 1 --repeat-penalty 1 --rope-scaling yarn --fit on --fit-target 512 --fit-ctx 262144 --parallel 6 --cont-batching --ctx-checkpoints 16 --reasoning on --verbose
+./llama-server -m $MODEL_DIR/$model --port $port --host $host -c $context -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP  --min-p $minP --top-k $topK --threads $threads --swa-full --kv-unified --cache-type-k q8_0 --cache-type-v q8_0 --batch-size 512 --ubatch-size 256  --presence-penalty 1 --repeat-penalty 1 --rope-scaling yarn --fit on --fit-target 512 --fit-ctx 262144 --parallel 8 --cont-batching --ctx-checkpoints 16 --reasoning on --verbose
 
 # --chat-template-kwargs '{"enable_thinking":true}'
