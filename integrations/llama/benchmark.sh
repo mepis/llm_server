fe316cfd862909d0520f2cce7e57a23a9f7c86d0
jon welling <jon.welling@ipqs.com>
2026-04-24 21:38:45 -0400

    Updated llama install.sh script

--- integrations/llama/benchmark.sh
+++ integrations/llama/benchmark.sh
@@ -36,6 +36,6 @@ export CUDACXX=$(which nvcc)
 # export LLAMA_ARG_FIT_CTX=262144
 # export LLAMA_ARG_FLASH_ATTN=1
 
-./llama-bench -m $MODEL_DIR/$MODEL -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --batch-size $batch_size --ubatch-size $ubatch_size --threads $threads --prio 2 --cpu-range 0-7 --cpu-strict 1
+./llama-bench -m $MODEL_DIR/$MODEL -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --batch-size $batch_size --ubatch-size $ubatch_size --threads $threads --prio 2 
 
 # --verbose
