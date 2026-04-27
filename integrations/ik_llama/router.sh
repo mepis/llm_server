79551c626a434fc41ffc7cdac4b7d150d048f726
jon welling <jon.welling@ipqs.com>
2026-04-27 09:31:57 -0400

    updated install script

--- integrations/ik_llama/router.sh
+++ integrations/ik_llama/router.sh
@@ -33,6 +33,6 @@ CURRENT_DIR=$(pwd)
 cd $CURRENT_DIR
 cd ik_llama.cpp/build/bin/
 
-./llama-server --models-dir $MODEL_DIR --models-autoload --models-max 2 --sleep-idle-seconds 30 --port $port --host $host -c $context -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching --min-p $minP --top-k $topK --threads $threads --kv-unified --cache-type-k q8_0 --cache-type-v q8_0 --batch-size $BATCH --ubatch-size $UBATCH --chat-template-kwargs '{"enable_thinking":true}' --parallel $PARALLEL --reasoning on --verbose --flash-attn 1 --repeat-penalty 1.0 --presence-penalty 1.0
+./llama-server --models $MODEL_DIR/Qwen3.6-27B-Q8_0.gguf --port $port --host $host -c $context -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching --min-p $minP --top-k $topK --threads $threads --kv-unified --cache-type-k q8_0 --cache-type-v q8_0 --batch-size $BATCH --ubatch-size $UBATCH --chat-template-kwargs '{"enable_thinking":true}' --parallel $PARALLEL --reasoning on --verbose --flash-attn 1 --repeat-penalty 1.0 --presence-penalty 1.0
 
 # --repeat-penalty 1.0 --presence-penalty 1.5 (1.0 - 1.5)  Use if looping is a problem
