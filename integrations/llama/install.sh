f3e6e0af14b167226a968703177152bad3d50cd0
jon welling <jon.welling@ipqs.com>
2026-04-27 21:28:22 -0400

    updated install script

--- integrations/llama/install.sh
+++ integrations/llama/install.sh
@@ -14,7 +14,7 @@ CURRENT_DIR=$(pwd)
 
 ## NEMOTRON ##
 ##--------
-# MODEL=Nemotron-3-Nano-30B.sh
+MODEL=Nemotron-3-Nano-30B.sh
 
 ## QWEN ##
 ##--------
@@ -25,7 +25,7 @@ CURRENT_DIR=$(pwd)
 # MODEL=Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-Q4_K_M.sh
 # MODEL=Qwen3.6-35B-A3B-MXFP4_MOE.sh
 # MODEL=Qwen3.6-35B-A3B-Q8_0.sh
-MODEL=Qwen3.6-27B-Q8_0.sh
+# MODEL=Qwen3.6-27B-Q8_0.sh
 
 rm run.sh
 echo -e "#!/bin/bash
