02316301fa50afdf6fbd4ad190a3dae738ffedf8
jon welling <jon.welling@ipqs.com>
2026-04-30 18:50:33 -0400

    updated  llama install script

--- integrations/llama/benchmark.sh
+++ integrations/llama/benchmark.sh
@@ -6,7 +6,7 @@ MODEL=Qwen3.6-27B-Q8_0.gguf
 
 # Benchmark configs
 batch_size=128,256,512,1024
-ubatch_size=256,512
+ubatch_size=128,256,512
 
 threads=20
 
@@ -31,15 +31,15 @@ export GGML_CUDA_ENABLE_UNIFIED_MEMORY=1
 export CUDACXX=$(which nvcc)
 
 ## added for benchmarks
-export LLAMA_ARG_MLOCK=on
-export LLAMA_ARG_SWA_FULL=on
-export LLAMA_ARG_MMAP=off
+# export LLAMA_ARG_MLOCK=on
+# export LLAMA_ARG_SWA_FULL=on
+# export LLAMA_ARG_MMAP=off
 export LLAMA_ARG_FIT=on
 export LLAMA_ARG_FIT_TARGET=256
 export LLAMA_ARG_FIT_CTX=262144
 export LLAMA_ARG_FLASH_ATTN=1
 export LLAMA_ARG_CONT_BATCHING=on
-export LLAMA_ARG_N_PREDICT=0
+# export LLAMA_ARG_N_PREDICT=0
 export LLAMA_ARG_N_PARALLEL=6
 
 ./llama-bench -m $MODEL_DIR/$MODEL -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --batch-size $batch_size --ubatch-size $ubatch_size --threads $threads --fit-target 512 --fit-ctx 262144 
