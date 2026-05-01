fd5b1c94e6e035736c0711c6cbd5d2c4f9dcc3db
jon welling <jon.welling@ipqs.com>
2026-04-30 21:17:05 -0400

    updated  llama install script

--- integrations/llama/router.sh
+++ integrations/llama/router.sh
@@ -4,7 +4,7 @@ export LLAMA_CACHE=$HOME/.llm_models
 export GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 
 export CUDACXX=$(which nvcc)
 export LLAMA_ARG_FIT=on
-export LLAMA_ARG_FIT_TARGET=523
+export LLAMA_ARG_FIT_TARGET=512
 export LLAMA_ARG_FIT_CTX=131072
 
 # Host Configs
@@ -34,6 +34,6 @@ CURRENT_DIR=$(pwd)
 cd $CURRENT_DIR
 cd llama.cpp/build/bin/
 
-./llama-server --models-dir $MODEL_DIR --models-autoload --models-max 1 --sleep-idle-seconds 30 --port $port --host $host -c $context -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching --min-p $minP --top-k $topK --threads $threads --kv-unified --cache-type-k q8_0 --cache-type-v q8_0 --batch-size $BATCH --ubatch-size $UBATCH --chat-template-kwargs '{"enable_thinking":true}' --parallel $PARALLEL --presence-penalty 1.5 
+./llama-server --models-dir $MODEL_DIR --models-autoload --models-max 1 --sleep-idle-seconds 30 --port $port --host $host -c $context -ngl 999 --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --temp $temp --top-p $topP --cont-batching --min-p $minP --top-k $topK --threads $threads --kv-unified --cache-type-k q8_0 --cache-type-v q8_0 --batch-size $BATCH --ubatch-size $UBATCH --chat-template-kwargs '{"enable_thinking":true}' --parallel $PARALLEL --presence-penalty 1.5 --fit-target 512 --fit-ctx 131072
 
 # --repeat-penalty 1.0 --presence-penalty 1.5 (1.0 - 1.5)  Use if looping is a problem
