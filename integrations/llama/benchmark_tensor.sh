9122cf8fc853bbe29098a2daffbce83405427960
jon welling <jon.welling@ipqs.com>
2026-05-12 08:11:32 -0400

    updated scripts

--- integrations/llama/benchmark_tensor.sh
+++ integrations/llama/benchmark_tensor.sh
@@ -33,19 +33,13 @@ MODEL=Qwen3.6-27B-Q8_0.gguf
 
 # EXTENSIVE TESTS CONFIGS
 #------------------------------------------------
-PROMPT_SIZE=256,512,1024,32768 # default=512
-GEN_SIZE=128,256,512,1024,8192 # default=128
-TEST_REPITITIONS=3 #default=5
+PROMPT_SIZE=256,512,1024,2048 # default=512
+GEN_SIZE=256,512,1024 # default=128
 
 MAIN_GPU=0
 T_SPLIT=16,12,12
 SPLIT_MODE=tensor
 LAYER_OFFLOAD=999
-
-# common contet size windows: 16384, 32768, 65536, 131072, 262144, 524288
-FIT_MIN_CTX=65536
-FIT_TARGET=512
-MEMORY_MAP=0,1
 
 CPU_THREADS=8
 CPU_STRICT=1
@@ -55,18 +49,13 @@ UBATCH_S=128,256,512,1024,2048
 
 FLASH_ATTENTION=1
 
-# valid values: q8_0, q4_0, q4_1, q5_0, q5_1, iq4_nl
-K_CACHE_TYPE=q8_0,q4_0
-V_CACHE_TYPE=q8_0,q4_0
-
-
 #----------------------------
 # ENV VALS
 #----------------------------
 # llama-bench does not update these automatically while benchmarking. The benchmark must be restarted manually after changing these values
 
-# CUDA_LAUNCH_QUEUE_SIZE=4x # Do not over do this, will cause memory issues and crash the system
-CUDA_UNIFIED_MEMORY=1 # set to 0 for single GPU machines
+CUDA_LAUNCH_QUEUE_SIZE=4x
+CUDA_UNIFIED_MEMORY=1 
 
 #----------------------------
 # BENCHMARK SCRIPT - DO NOT MODIFY UNLESS NEEDED, USE VARIABLES ABOVE
@@ -86,10 +75,6 @@ systemctl --user stop llama.service
 cd $CURRENT_DIR
 cd llama.cpp/build/bin/
 
-# QUICK TEST
+# Run Benchmark
 #------------------------------------------------
-# ./llama-bench -m $MODEL_DIR/$MODEL -ngl $LAYER_OFFLOAD --split-mode $SPLIT_MODE --tensor-split $T_SPLIT --main-gpu $MAIN_GPU --batch-size $BATCH_S --ubatch-size $UBATCH_S --fit-target $FIT_TARGET --fit-ctx $FIT_MIN_CTX --n-prompt $PROMPT_SIZE --n-gen $GEN_SIZE --flash-attn $FLASH_ATTENTION --repetitions $TEST_REPITITIONS --mmap $MEMORY_MAP 
-
-# EXTENSIVE TESTS CONFIGS
-#------------------------------------------------
-./llama-bench -m $MODEL_DIR/$MODEL -ngl $LAYER_OFFLOAD --split-mode $SPLIT_MODE --tensor-split $T_SPLIT --main-gpu $MAIN_GPU --batch-size $BATCH_S --ubatch-size $UBATCH_S --threads $CPU_THREADS --cpu-strict $CPU_STRICT --verbose --flash-attn $FLASH_ATTENTION
+./llama-bench -m $MODEL_DIR/$MODEL -ngl $LAYER_OFFLOAD --split-mode $SPLIT_MODE --tensor-split $T_SPLIT --main-gpu $MAIN_GPU --batch-size $BATCH_S --ubatch-size $UBATCH_S --threads $CPU_THREADS --cpu-strict $CPU_STRICT --flash-attn $FLASH_ATTENTION
