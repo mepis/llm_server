179e23b8fc34f6915e79308b7402322251f91367
jon welling <jon.welling@ipqs.com>
2026-05-15 21:31:38 -0400

    script update

--- integrations/llama/models/Qwen3.6-35B-A3B-Q8_0.sh
+++ integrations/llama/models/Qwen3.6-35B-A3B-Q8_0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# model=Qwen3.6-35B-A3B-Q8_0.gguf
-model=Qwen3.6-35B-A3B-UD-IQ4_NL_XL.gguf
+model=Qwen3.6-35B-A3B-Q8_0.gguf
+# model=Qwen3.6-35B-A3B-UD-IQ4_NL_XL.gguf
 mmproj=qwen3.6-35b-mmproj-f16.gguf
 
 # Host Configs
