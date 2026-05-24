fbd775f7ec1f79746b12d839192e2f13d92ea6c1
jon welling <jon.welling@ipqs.com>
2026-05-24 17:08:37 -0400

    update scripts

--- integrations/llama/models/Qwen3.6-35B-A3B-Q8_0.sh
+++ integrations/llama/models/Qwen3.6-35B-A3B-Q8_0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # model=Qwen3.6-35B-A3B-UD-Q8_K_XL.gguf
-model=Qwen3.6-35B-A3B-Q8_0.gguf.1 # mtp model
-# model=Qwen3.6-35B-A3B-Q8_0.gguf
+# model=Qwen3.6-35B-A3B-Q8_0.gguf.1 # mtp model
+model=Qwen3.6-35B-A3B-Q8_0.gguf
 # model=Qwen3.6-35B-A3B-MXFP4_MOE.gguf
 # model=Qwen3.6-35B-A3B-UD-IQ4_NL_XL.gguf
 mmproj=qwen3.6-35b-mmproj-f16.gguf
@@ -41,8 +41,8 @@ export LLAMA_CACHE=$MODEL_CACHE
 export GGML_CUDA_ENABLE_UNIFIED_MEMORY=1 
 export CUDACXX=$(which nvcc)
 export LLAMA_ARG_FIT=on
-# export LLAMA_ARG_FIT_TARGET=1024
-# export LLAMA_ARG_FIT_CTX=262144
+export LLAMA_ARG_FIT_TARGET=256
+export LLAMA_ARG_FIT_CTX=131072
 # export GGML_CUDA_P2P=on
 
 ./llama-server -m $MODEL_DIR/$model --mmproj $MODEL_DIR/$mmproj --port $port --host $host -c $context -ngl 999 --cont-batching --temp $temp --top-p $topP  --min-p $minP --top-k $topK --batch-size 256 --ubatch-size 256 --kv-unified --flash-attn on --reasoning on --cache-prompt --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --cpu-range 0-7 --cpu-strict-batch 1 --threads-batch 8 --threads $threads --cpu-strict 1 
