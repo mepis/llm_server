1a7c4e8cf45342bd598e31ad3e312e038576db89
jon welling <jon.welling@ipqs.com>
2026-05-25 08:57:17 -0400

    update scripts

--- integrations/llama/models/Qwen3.6-27B-Q8_0-mtp.sh
+++ integrations/llama/models/Qwen3.6-27B-Q8_0-mtp.sh
@@ -42,6 +42,6 @@ export LLAMA_ARG_FIT=on
 # export LLAMA_ARG_FIT_CTX=262144
 export GGML_CUDA_P2P=on
 
-./llama-server -m $MODEL_DIR/$model --mmproj $MODEL_DIR/$mmproj --port $port --host $host -c $context -ngl 999 --cont-batching --temp $temp --top-p $topP  --min-p $minP --top-k $topK --batch-size 512 --ubatch-size 256 --kv-unified --flash-attn on --reasoning on --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --cpu-range 0-7 --cpu-strict-batch 1 --threads-batch 8 --threads $threads --cpu-strict 1 --spec-type draft-mtp --spec-draft-n-max 3 --rope-scaling yarn --rope-scale 2.0 --cache-prompt
+./llama-server -m $MODEL_DIR/$model --mmproj $MODEL_DIR/$mmproj --port $port --host $host -c $context -ngl 999 --cont-batching --temp $temp --top-p $topP  --min-p $minP --top-k $topK --batch-size 512 --ubatch-size 256 --kv-unified --flash-attn on --reasoning on --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --cpu-range 0-7 --cpu-strict-batch 1 --threads-batch 8 --threads $threads --cpu-strict 1 --spec-type draft-mtp --spec-draft-n-max 3 --rope-scaling yarn --rope-scale 2.0 --repeat-penalty 1.0
 
 # --cache-type-k q8_0 --cache-type-v q8_0 --repeat-penalty 0.0 --presence-penalty 0.0 --rope-scaling yarn --rope-scale 2.0 --cache-prompt
