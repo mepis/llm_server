116695fe959e15cd3a1b8ea139f4fbb834947e8d
jon welling <jon.welling@ipqs.com>
2026-05-27 22:13:16 -0400

    update scripts

--- integrations/llama/models/Qwen3.6-35B-A3B-Q8_0.sh
+++ integrations/llama/models/Qwen3.6-35B-A3B-Q8_0.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # model=Qwen3.6-35B-A3B-UD-Q8_K_XL.gguf
 model=Qwen3.6-35B-A3B-Q8_0.gguf.1 # mtp model
+# model=Qwen3.6-35B-A3B-UD-Q6_K_XL.gguf # mtp model
 # model=Qwen3.6-35B-A3B-Q8_0.gguf
 # model=Qwen3.6-35B-A3B-MXFP4_MOE.gguf
 # model=Qwen3.6-35B-A3B-UD-IQ4_NL_XL.gguf
