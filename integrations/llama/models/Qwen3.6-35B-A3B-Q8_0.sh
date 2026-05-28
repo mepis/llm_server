4f28a6a234c23a0063d0f9d40284904b921fee28
jon welling <jon.welling@ipqs.com>
2026-05-27 22:06:24 -0400

    update scripts

--- integrations/llama/models/Qwen3.6-35B-A3B-Q8_0.sh
+++ integrations/llama/models/Qwen3.6-35B-A3B-Q8_0.sh
@@ -19,7 +19,7 @@ threads=8
 # Model Configs
 # common contet size windows: 16384, 32768, 65536, 131072, 262144, 524288
 context=262144
-temp=0.6
+temp=0.7
 topP=0.95
 minP=0.00
 topK=20
@@ -45,7 +45,7 @@ export LLAMA_ARG_FIT_TARGET=256
 export LLAMA_ARG_FIT_CTX=65536
 # export GGML_CUDA_P2P=on
 
-./llama-server -m $MODEL_DIR/$model --mmproj $MODEL_DIR/$mmproj --port $port --host $host -c $context -ngl 999 --cont-batching --temp $temp --top-p $topP  --min-p $minP --top-k $topK --batch-size 256 --ubatch-size 256 --flash-attn on --reasoning on --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --cpu-range 0-7 --cpu-strict-batch 1 --threads-batch 8 --threads $threads --cpu-strict 1 --spec-type draft-mtp --spec-draft-n-max 3  --cache-type-k q8_0 --cache-type-v q8_0 --repeat-penalty 1.0
+./llama-server -m $MODEL_DIR/$model --mmproj $MODEL_DIR/$mmproj --port $port --host $host -c $context -ngl 999 --cont-batching --temp $temp --top-p $topP  --min-p $minP --top-k $topK --batch-size 256 --ubatch-size 256 --flash-attn on --reasoning on --split-mode $splitMode --tensor-split $tensorSplit --main-gpu $mainGpu --cpu-range 0-7 --cpu-strict-batch 1 --threads-batch 8 --threads $threads --cpu-strict 1 --spec-type draft-mtp --spec-draft-n-max 3  --cache-type-k q8_0 --cache-type-v q8_0 --repeat-penalty 1.1
 
 
 # --cache-type-k q8_0 --cache-type-v q8_0 --repeat-penalty 0.0 --mmproj $MODEL_DIR/$mmproj --rope-scaling yarn --rope-scale 2.0 --presence-penalty 0.0 --repeat-penalty 1.0 --jinja --no-mmap --spec-type draft-mtp --spec-draft-n-max 3
