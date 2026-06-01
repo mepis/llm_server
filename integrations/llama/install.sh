7065697eafa6bd21d97c0f358ff5fcde63c3e8af
jon welling <jon.welling@ipqs.com>
2026-06-01 19:27:23 -0400

    updated scripts

--- integrations/llama/install.sh
+++ integrations/llama/install.sh
@@ -9,8 +9,8 @@ CURRENT_DIR=$(pwd)
 # MODELS
 # -----------------
 # MODEL=router.sh
-# MODEL=Qwen3.6-27B-Q8_0.sh
-MODEL=Qwen3.6-35B-A3B-Q8_0.sh
+MODEL=Qwen3.6-27B-Q8_0.sh
+# MODEL=Qwen3.6-35B-A3B-Q8_0.sh
 # MODEL=Qwen3.6-27B-Q8_0-mtp.sh
 
 rm run.sh
